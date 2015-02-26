33c704e0c720cd15e0f43f37318b55b6cb8a533d
WunGCQ <wangchunqibuaa@163.com>
2015-02-26 18:35:02 +0800

    大更新
    完成模板系统，初步完成用户系统，写完核心的路由控制的模板和数据模型的模板

--- public/javascripts/Models/GroupModel.js
+++ public/javascripts/Models/GroupModel.js
@@ -1,3 +1,17 @@
 /**
  * Created by wungcq on 15/2/15.
  */
+window.GroupModel = function(){
+    return this;
+};
+
+//Group 继承自数据模型类
+GroupModel.prototype = new Model();
+//需要设置每个数据模型的增删改查路径
+(function(){
+    GroupModel.prototype.templatePath = 'http://localhost:63342/github/ngtest/public/templates/group.html';
+    GroupModel.prototype.AddPath      = 'http://localhost:63342/github/ngtest/public/JSON/register.json';
+    GroupModel.prototype.RetrievePath = 'http://localhost:63342/github/ngtest/public/JSON/get_user.json';
+    GroupModel.prototype.UpdatePath   = 'http://localhost:63342/github/ngtest/public/JSON/update_uer.json';
+    GroupModel.prototype.LogoutPath   = 'http://localhost:63342/github/ngtest/public/JSON/logout.json';
+})();
--- public/javascripts/Models/ProblemModel.js
+++ public/javascripts/Models/ProblemModel.js
@@ -1,3 +1,16 @@
 /**
  * Created by wungcq on 15/2/15.
  */
+window.ProblemModel = function(){
+    return this;
+};
+
+//Problem 继承自数据模型类
+ProblemModel.prototype = new Model();
+//需要设置每个数据模型的增删改查路径
+(function(){
+    ProblemModel.prototype.templatePath = 'http://localhost:63342/github/ngtest/public/templates/problem.html';
+    ProblemModel.prototype.AddPath      = 'http://localhost:63342/github/ngtest/public/JSON/create_problem.json';
+    ProblemModel.prototype.RetrievePath = 'http://localhost:63342/github/ngtest/public/JSON/get_problem.json';
+    ProblemModel.prototype.UpdatePath   = 'http://localhost:63342/github/ngtest/public/JSON/update_problem.json';
+})();
